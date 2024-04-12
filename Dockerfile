27825326bcc672f67ecdb968408d064a87585053
initialencounter <2911583893@qq.com>
2024-04-13 03:23:41 +0800

    fix: arch

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,16 @@
 FROM initialencounter/napcat:base
 
-COPY NapCat.linux.x64.zip /tmp/NapCat.linux.x64.zip
+COPY NapCat.linux.zip /tmp/NapCat.linux.zip
 COPY config.txt entrypoint.sh /root/
 
 # 安装Linux QQ
-RUN curl -o /root/linuxqq.deb https://dldir1.qq.com/qqfile/qq/QQNT/Linux/QQ_3.2.7_240410_amd64_01.deb && \
+RUN arch=$(arch | sed s/aarch64/arm64/ | sed s/x86_64/amd64/) && \
+    curl -o /root/linuxqq.deb https://dldir1.qq.com/qqfile/qq/QQNT/Linux/QQ_3.2.7_240410_{arch}_01.deb && \
     dpkg -i --force-depends /root/linuxqq.deb && rm /root/linuxqq.deb && \
 
     # 安装 napcat
-    unzip /tmp/NapCat.linux.x64.zip -d /root/ && \
-    rm /tmp/NapCat.linux.x64.zip && \
+    unzip /tmp/NapCat.linux.zip -d /root/ && \
+    rm /tmp/NapCat.linux.zip && \
 
     chmod +x /root/napcat.sh && \
     chmod +x /root/entrypoint.sh && \
--- get_artifacts.sh
+++ get_artifacts.sh
@@ -3,7 +3,8 @@
 # 设置仓库信息
 repository="NapNeko/NapCat.Build"
 run_id="8662095740"
-artifact_name="NapCat.linux.x64"
+arch=$(arch | sed s/aarch64/arm64/ | sed s/x86_64/x64/)
+artifact_name="NapCat.linux.{$arch}"
 token=""
 
 # 设置输出目录
@@ -13,6 +14,6 @@ output_dir="."
 artifact_id=$(curl -s -X GET -H "Authorization: token $token" "https://api.github.com/repos/$repository/actions/runs/$run_id/artifacts" | jq -r ".artifacts[] | select(.name == \"$artifact_name\") | .id")
 
 # 下载artifact
-curl -s -X GET -H "Authorization: token $token" -L "https://api.github.com/repos/$repository/actions/artifacts/$artifact_id/zip" -o "$output_dir/$artifact_name.zip"
+curl -s -X GET -H "Authorization: token $token" -L "https://api.github.com/repos/$repository/actions/artifacts/$artifact_id/zip" -o "$output_dir/NapCat.linux.zip"
 
-echo "编译产物已保存到$output_dir/$artifact_name.zip"
+echo "编译产物已保存到$output_dir/NapCat.linux.zip"
