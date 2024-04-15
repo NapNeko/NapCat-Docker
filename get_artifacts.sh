1b899bcb682007a1d91e62279bfd322da564c6fc
手瓜一十雪 <nanaeonn@outlook.com>
2024-04-15 11:41:40 +0800

    fix

--- get_artifacts.sh
+++ get_artifacts.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# 设置仓库信息
-repository="NapNeko/NapCat.Build"
 arch=$(arch | sed s/aarch64/arm64/ | sed s/x86_64/x64/)
 artifact_name="NapCat.linux.$arch"
 token="$1"
@@ -10,6 +8,6 @@ token="$1"
 output_dir="."
 
 # 下载release
-curl -s -X GET -H "Authorization: token $token" -L "https://github.com/NapNeko/NapCatQQ/releases/download/v1.0.2/NapCat.linux.$arch.zip" -o "$output_dir/NapCat.linux.zip"
-
+curl -s -X GET -H "Authorization: token $token" -L "https://github.com/NapNeko/NapCatQQ/releases/download/v1.0.2/$artifact_name.zip" -o "$output_dir/NapCat.linux.zip"
+ls
 echo "编译产物已保存到$output_dir/NapCat.linux.zip"
