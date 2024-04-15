465e467212e993d3725990e46e9e3a543c767a88
手瓜一十雪 <nanaeonn@outlook.com>
2024-04-15 12:27:16 +0800

    fix

--- get_artifacts.sh
+++ get_artifacts.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-arch=$(arch | sed s/aarch64/arm64/ | sed s/x86_64/x64/)
-artifact_name="NapCat.linux.$arch"
+[ "$ARCH_NC" = "linux/amd64" ] && ARCH_DOWN="x64" || [ "$ARCH_NC" = "linux/arm64" ] && ARCH_DOWN="arm64"
 token="$1"
-
+artifact_name="NapCat.linux.$ARCH_DOWN"
 # 设置输出目录
 output_dir="."
 
