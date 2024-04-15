8b2431ec8e0273e2dd15d61023cd8b5fcbb1894f
手瓜一十雪 <nanaeonn@outlook.com>
2024-04-15 11:27:54 +0800

    fix

--- get_artifacts.sh
+++ get_artifacts.sh
@@ -10,6 +10,6 @@ token="$1"
 output_dir="."
 
 # 下载release
-curl -s -X GET -H "Authorization: token $token" -L "https://github.com/NapNeko/NapCatQQ/releases/download/v1.0.1/NapCat.linux.$arch.zip" -o "$output_dir/NapCat.linux.zip"
+curl -s -X GET -H "Authorization: token $token" -L "https://github.com/NapNeko/NapCatQQ/releases/download/v1.0.2/NapCat.linux.$arch.zip" -o "$output_dir/NapCat.linux.zip"
 
 echo "编译产物已保存到$output_dir/NapCat.linux.zip"
