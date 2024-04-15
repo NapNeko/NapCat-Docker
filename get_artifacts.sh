ebd0cdcdbf8424b7424ff9dede5ef86604b4b907
手瓜一十雪 <nanaeonn@outlook.com>
2024-04-15 12:37:44 +0800

    fix

--- get_artifacts.sh
+++ get_artifacts.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-[ "$ARCH_NC" = "linux/amd64" ] && ARCH_DOWN="x64" || [ "$ARCH_NC" = "linux/arm64" ] && ARCH_DOWN="arm64"
+ARCH_DOWN=$(echo $ARCH_NC | sed 's/linux\/amd64/x64/' | sed 's/linux\/arm64/arm64/')
 token="$1"
 artifact_name="NapCat.linux.$ARCH_DOWN"
 # 设置输出目录
 output_dir="."
-
+echo "当前架构$ARCH_DOWN"
 # 下载release
 curl -s -X GET -H "Authorization: token $token" -L "https://github.com/NapNeko/NapCatQQ/releases/download/v1.0.2/$artifact_name.zip" -o "$output_dir/NapCat.linux.zip"
 ls
