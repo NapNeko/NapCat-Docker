6c7569421b7027ed354a0be21152156be4ad65e5
手瓜一十雪 <nanaeonn@outlook.com>
2024-04-15 11:21:11 +0800

    fix

--- get_artifacts.sh
+++ get_artifacts.sh
@@ -2,7 +2,6 @@
 
 # 设置仓库信息
 repository="NapNeko/NapCat.Build"
-run_id="8674568648"
 arch=$(arch | sed s/aarch64/arm64/ | sed s/x86_64/x64/)
 artifact_name="NapCat.linux.$arch"
 token="$1"
@@ -10,10 +9,7 @@ token="$1"
 # 设置输出目录
 output_dir="."
 
-# 获取artifact ID
-artifact_id=$(curl -s -X GET -H "Authorization: token $token" "https://api.github.com/repos/$repository/actions/runs/$run_id/artifacts" | jq -r ".artifacts[] | select(.name == \"$artifact_name\") | .id")
-
-# 下载artifact
-curl -s -X GET -H "Authorization: token $token" -L "https://api.github.com/repos/$repository/actions/artifacts/$artifact_id/zip" -o "$output_dir/NapCat.linux.zip"
+# 下载release
+curl -s -X GET -H "Authorization: token $token" -L "https://github.com/NapNeko/NapCatQQ/releases/download/v1.0.1/NapCat.linux.$arch.zip" -o "$output_dir/NapCat.linux.zip"
 
 echo "编译产物已保存到$output_dir/NapCat.linux.zip"
