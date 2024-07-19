c313ddbaa4d63a3020ae762bb731830072c00070
WWWw <109729945+initialencounter@users.noreply.github.com>
2024-07-19 13:11:50 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -89,4 +89,4 @@ Xvfb :1 -screen 0 1080x760x16 +extension GLX +render &
 export FFMPEG_PATH=/usr/bin/ffmpeg
 export DISPLAY=:1
 cd ./napcat
-qq --no-sandbox --disable-gpu --headless -q $ACCOUNT
+qq --no-sandbox -q $ACCOUNT
